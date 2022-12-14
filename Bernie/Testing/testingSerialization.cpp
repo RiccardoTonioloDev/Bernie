b5630fc7226fcf7c786ffe772daa92620f52665c
kara <ssctonioloriccardo@gmail.com>
2022-12-14 10:24:34 +0100

    Completed unit testing functions (completely working), and added testing for accounts.

--- Bernie/Testing/testingSerialization.cpp
+++ Bernie/Testing/testingSerialization.cpp
@@ -5,65 +5,52 @@
 #include "../Models/Note.h"
 #include <utility>
 #include <vector>
+#include <iostream>
 
 void serializationTest(SerializableObject& input, const std::string& expected, const std::string& testName){
     std::string serializedObj = input.serialize();
     if(serializedObj == expected){
-        std::cout << "TEST (" + testName + "): PASSED";
-    }else std::cout << "TEST (" + testName + "): NOT PASSED";
+        std::cout << "TEST (" + testName + "): \033[32mPASSED\033[0m";
+    }else std::cout << "TEST (" + testName + "): \033[31mNOT PASSED\033[0m";
     std::cout << std::endl;
 }
 void deSanitizationTest(std::string input, const std::pair<bool,std::vector<std::string>>& expected, const std::string& testName){
     std::pair<bool,std::vector<std::string>> couple = SerializableObject::deSanitize(input);
     if(expected.first != couple.first){
-        std::cout << "TEST (" + testName + "): NOT PASSED";
+        std::cout << "TEST (" + testName + "): \033[31mNOT PASSED\033[0m";
     }else{
        std::vector<std::string>::const_iterator citExpected = expected.second.begin();
        std::vector<std::string>::const_iterator citCouple = couple.second.begin();
        bool areEqual = true;
        while(areEqual && citExpected!=expected.second.end() && citCouple != couple.second.end()){
-           if(citCouple != citExpected) areEqual = false;
+           if(*citCouple != *citExpected){
+               std::cout << std::endl;
+               areEqual = false;
+           }
+           ++citCouple;
+           ++citExpected;
        }
        if (areEqual){
-           std::cout << "TEST (" + testName + "): NOT PASSED";
+           std::cout << "TEST (" + testName + "): \033[32mPASSED\033[0m";
        }else{
-            std::cout << "TEST (" + testName + "): NOT PASSED";
+           std::cout << "TEST (" + testName + "): \033[31mNOT PASSED\033[0m";
        }
     }
+    std::cout << std::endl;
 }
 
 int main(){
+    std::cout << "//Account Unit Testing -------------------------------------------------------------------------------"<<std::endl;
     Account p1("NameProva1","EmailProva1","PasswordProva1","UsernameProva1");
-    serializationTest(p1,std::string("ACCOUNT,NameProva1,EmailProva1,PasswordProva1,UsernameProva1"),std::string("Account 1"));
-    deSanitizationTest(p1.serialize(),std::make_pair<bool,std::vector<std::string>>())
-    /*
-    std::cout << p1.serialize() << std::endl;
-    std::pair<bool, std::vector<std::string>> tuple = SerializableObject::deSanitize(p1.serialize());
-    std::cout << "Is corrupted? ";
-    std::cout << (std::get<0>(tuple) ? "True" : "False") << std::endl;
-    std::cout << "VALUES:" << std::endl;
-    for(std::vector<std::string>::const_iterator cit = std::get<1>(tuple).begin();cit != std::get<1>(tuple).end();++cit){
-        std::cout << *cit << std::endl;
-    }
-     */
-    std::cout << "###################################################################################" << std::endl;
+    serializationTest(p1,std::string("ACCOUNT,NameProva1,EmailProva1,PasswordProva1,UsernameProva1"),std::string("serialization Account 1"));
+    deSanitizationTest(p1.serialize(),std::make_pair<bool,std::vector<std::string>>(false,std::vector<std::string>{"ACCOUNT","NameProva1","EmailProva1","PasswordProva1","UsernameProva1"}),"deSanitization Account 1");
     Account pNotCorrupted("&,,",",&&&,","&&,&,&,","&&&&,&,&&,");
-    serializationTest(pNotCorrupted,std::string("ACCOUNT,&&&,&,,&,&&&&&&&,,&&&&&,&&&,&&&,,&&&&&&&&&,&&&,&&&&&,"),std::string("Stressing on escape and commas"));
-    std::cout << pNotCorrupted.serialize() << std::endl;
-    std::pair<bool, std::vector<std::string>> tupleNotCorrupted = SerializableObject::deSanitize(pNotCorrupted.serialize());
-    std::cout << "Is corrupted? ";
-    std::cout << (std::get<0>(tupleNotCorrupted) ? "True" : "False") << std::endl;
-    std::cout << "VALUES:" << std::endl;
-    for(std::vector<std::string>::const_iterator cit = std::get<1>(tupleNotCorrupted).begin();cit != std::get<1>(tupleNotCorrupted).end();++cit){
-        std::cout << *cit << std::endl;
-    }
-    std::cout << "###################################################################################" << std::endl;
+    serializationTest(pNotCorrupted,std::string("ACCOUNT,&&&,&,,&,&&&&&&&,,&&&&&,&&&,&&&,,&&&&&&&&&,&&&,&&&&&,"),std::string("serialization stressing on escape and commas"));
+    deSanitizationTest(pNotCorrupted.serialize(),std::make_pair<bool,std::vector<std::string>>(false,std::vector<std::string>{"ACCOUNT","&,,",",&&&,","&&,&,&,","&&&&,&,&&,"}),"deSanitization Account notCorrupted");
     std::string corrupted("ACCOUNT,NameProva2,Email&Prova2,PasswordProva2,UsernameProva2");
-    std::pair<bool, std::vector<std::string>> tupleCorrupted = SerializableObject::deSanitize(corrupted);
-    std::cout << "Is corrupted? ";
-    std::cout << (std::get<0>(tupleCorrupted) ? "True" : "False") << std::endl;
-    std::cout << "VALUES:" << std::endl;
-    for(std::vector<std::string>::const_iterator cit = std::get<1>(tupleCorrupted).begin();cit != std::get<1>(tupleCorrupted).end();++cit){
-        std::cout << *cit << std::endl;
-    }
+    deSanitizationTest(corrupted,std::make_pair<bool,std::vector<std::string>>(true,std::vector<std::string>{}),"deSanitization Account Corrupted");
+    std::cout << "//CreditCard Unit Testing ----------------------------------------------------------------------------"<<std::endl;
+    std::cout << "//Contacts Unit Testing ------------------------------------------------------------------------------"<<std::endl;
+    std::cout << "//CryptoWallet Unit Testing --------------------------------------------------------------------------"<<std::endl;
+    std::cout << "//Note Unit Testing ----------------------------------------------------------------------------------"<<std::endl;
 }
