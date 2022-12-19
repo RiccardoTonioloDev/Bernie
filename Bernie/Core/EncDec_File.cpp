bfa57855dd6ad674a6647c428723a5fe8bda9a2c
kara <ssctonioloriccardo@gmail.com>
2022-12-19 18:21:44 +0100

    Started working on EncDec_File.

---
+++ Bernie/Core/EncDec_File.cpp
@@ -0,0 +1,5 @@
+//
+// Created by Riccardo Toniolo on 19/12/22.
+//
+
+#include "EncDec_File.h"
---
+++ Bernie/Core/EncDec_File.h
@@ -0,0 +1,18 @@
+#ifndef ENCDEC_FILE_H
+#define ENCDEC_FILE_H
+#include <string>
+#include "RBBSTree.h"
+
+
+class EncDec_File {
+private:
+    std::string key;
+    std::string fileName;
+public:
+    EncDec_File(const std::string& k, const std::string& fN);
+    bool fileExists() const;
+    void encInFile(const std::string& inputToEnc);
+};
+
+
+#endif //ENCDEC_FILE_H
